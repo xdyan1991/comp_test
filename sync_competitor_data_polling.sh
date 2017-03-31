03457dd9cdfa2e34fdf615252e26b3e37f00e36f
xdyan1991 <xdyan_1991@163.com>
2017-03-31 11:59:31 +0800

    use aws`s full path

--- sync_competitor_data_polling.sh
+++ sync_competitor_data_polling.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
+rm -rf /data/competitor/*
 mkdir /data/competitor/
-aws s3 sync s3://emr-gift-sin/daily_competitor_user_id/sync/ /data/competitor/ --exclude *$
+/usr/local/bin/aws s3 sync s3://emr-gift-sin/daily_competitor_user_id/sync/ /data/competitor/ --exclude *$
